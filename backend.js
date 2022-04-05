fca0182c300511860b6c0b307bb51f3c359ffc2a
ljn9888 <ljn9888@yeah.net>
2022-04-05 18:01:41 -0400

    update new version

--- backend.js
+++ backend.js
@@ -24,13 +24,22 @@ app.get('/', function (req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
  res.send(str);
 })
- 
+
+app.post('/', function (req, res) {
+  // console.log(req.body);
+  // console.log(msg);
+  //  res.send('Hello Worssssld');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+ res.send('hahahahaha');
+})
+
 var server = app.listen(8082, function () {
- 
+
   var host = server.address().address
   var port = server.address().port
   console.log("应用实例，访问地址为 http://%s:%s", host, port)
  
+
 })
 
 ls.stderr.on('data', (data) => {
--- test.js
+++ test.js
@@ -1,15 +1,42 @@
-const { spawn } = require('child_process');
-const ls = spawn('python3', ['fuzz.py', 'https://uwaterloo.ca/coronavirus']);
 
-ls.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
+var msg;
+var express = require('express');
+// var cors = require('cors');
+var app = express();
 
-ls.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`);
-});
+app.all('/', function (req, res) {
+  // console.log(req.body);
+  // console.log(msg);
+  //  res.send('Hello Worssssld');
+  console.log(req.headers.url);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST', 'OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+ res.send('hehee');
+})
 
-ls.on('close', (code) => {
-  console.log(`child process exited with code $[code]`);
-});
+app.all('/sendurl',  (req, res)=> {
+  // console.log(req.body);
+  // console.log(msg);
+  //  res.send('Hello Worssssld');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+ res.send('sendurl');
+})
+// app.post('/', function (req, res) {
+//   console.log(req.body);
+//   // console.log(msg);
+//   //  res.send('Hello Worssssld');
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//  res.send('hahahahaha');
+// })
+
+var server = app.listen(8082, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+ 
+
+})
+
 
