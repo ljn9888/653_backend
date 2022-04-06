be9914222ea0610a08664ffdc52ee18e7343cf39
ljn9888 <ljn9888@yeah.net>
2022-04-06 15:52:16 -0400

    p

--- test.js
+++ test.js
@@ -11,7 +11,7 @@ app.all('/', function (req, res) {
   console.log(req.headers.url);
   res.setHeader('Access-Control-Allow-Origin', '*');
   // res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST', 'OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', '    *');
+  res.setHeader('Access-Control-Allow-Headers', '*');
  res.send('hehe   e');
 })
 
